2b264ab0cb8788b9b0c51a964944a7c2750d19d4
yoitshavi <aboyer@appdev.yearup.org>
2023-04-25 13:26:45 -0400

    Committed number 4

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,9 @@
+var rule = 72
+var rateOfReturn = 3
+var money = 1000
+var doubleMoney = money * 2
+
+var time = rule / rateOfReturn
+
+
+console.log('At a',rateOfReturn,'% interest rate, your savings account will be worth',doubleMoney,'in',time,' years.')

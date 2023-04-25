dc871d9b1cccb6b818db13293eed60d4eaf2d507
yoitshavi <aboyer@appdev.yearup.org>
2023-04-25 10:45:04 -0400

    Commited exercise 1 and 2

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,9 @@
+var home = 100000.00;
+var car = 10000.00;
+
+var personalLoan = 2558.70;
+var studentLoan = 50000.00;
+
+var netWorth = (home + car) - (personalLoan + studentLoan);
+
+console.log('Your net worth is', netWorth.toFixed(2));
---
+++ tips.js
@@ -0,0 +1,9 @@
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.00;
+
+var totalDue = foodCost + tax + tip;
+
+console.log('Food cost is',foodCost, 'and tax is', tax);
+console.log('Tip is', tip.toFixed(2));
+console.log('total due is', totalDue);
